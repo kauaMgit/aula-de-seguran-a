5fce9489d1d3da3603d0fc76ec581522ccf0758b
Kaua Maciel De Oliveira Silva <00001118524263sp@aluno.educacao.sp.gov.br>
2024-08-30 14:54:20 -0300

    oi

---
+++ listadenumero.py
@@ -0,0 +1,11 @@
+lista = []
+for i in range(10):
+    n = int(input('digite um numero.'))
+    lista.append(n)
+    soma = sum(lista)
+    maior = max (lista)
+    menor = min(lista)
+    print('some:',soma )
+    print("maior",maior)
+    print('menor',menor)
+#comentar 
